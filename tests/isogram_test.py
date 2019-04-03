dc2dc733cfa6ad93a6f6e78e098f68ab1849b27d
Karanveer Plaha <karan@kv-mac.local>
2019-04-02 21:36:07 -0400

    Use pytest instead of unittest and tests directory

---
+++ tests/isogram_test.py
@@ -0,0 +1,19 @@
+import unittest
+from kata.isogram import is_isogram
+
+
+def test_isogram_returns_true():
+    test_input = 'isogram'
+    assert is_isogram(test_input)
+
+def test_empty_string_returns_true():
+    test_input = ''
+    assert is_isogram(test_input)
+
+def test_ignores_case_repeat_returns_false():
+    test_input = 'moOse'
+    assert not is_isogram(test_input)
+
+def test_repeat_returns_false():
+    test_input = 'aba'
+    assert not is_isogram(test_input)
---
+++ tests/remove_spaces_test.py
@@ -0,0 +1,12 @@
+import unittest
+from kata.remove_spaces import no_space
+
+def test_1():
+    expected = 'nospaces'
+    test_input = 'n o   sp  a c   e s'
+    assert no_space(test_input) == expected
+
+def test_2():
+    expected = 'thishasnospaceseither'
+    test_input = 't h is ha s no spa  ce s e  i th  er      '
+    assert no_space(test_input) == expected
